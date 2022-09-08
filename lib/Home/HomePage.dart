980b3727fe2b1049151475220e9e2b45b3d425bf
mohamedrayelalmi <rayenalmi64@gmail.com>
2022-09-08 19:41:35 +0100

    Excusives Home

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -307,7 +307,334 @@ class _HomePage_State extends State<HomePage> {
         ),
       ),
 
-      // drawer: Drawer(),
+      body : SingleChildScrollView(
+          padding: EdgeInsets.only(left: 33),
+          child: Column ( children: [
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                Text(
+                  "Excusives",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF120D26),
+                  ),),
+                  SizedBox(width: 121,),
+                Text(
+                  "Voir tout",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF747688),
+                  ),),
+                Icon(Icons.arrow_right , size: 25,color: Color(0xFF747688),),
+              ],
+            ),
+            SingleChildScrollView(
+            //changing scroll direction into horizontal
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+                getCardEvent(),
+                SizedBox(width: 10,),
+                getCardEvent(),
+                SizedBox(width: 10,),
+                getCardEvent(),
+                SizedBox(width: 10,),
+                getCardEvent(),
+                SizedBox(width: 10,),
+                getCardEvent(),
+                SizedBox(width: 10,),
+                getCardEvent(),
+              ],
+            ),),
+          ], )
+      ),
+
+    );
+  }
+}
+
+
+
+Widget getCardEvent()
+{
+  return Container(
+      height: 255,
+      width: 237,
+      padding: EdgeInsets.only(
+        left: 9.0,
+        right: 9.0,
+        top: 10,
+        //bottom: 5,
+      ),
+      decoration:
+      new BoxDecoration(
+        borderRadius: BorderRadius.circular(13.0),
+        color: Colors.white,
+      ),
+      child: Column (
+        children: [
+          Stack(
+            children: <Widget>[
+              Container(
+                child:  ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child:
+                  Image.asset(
+                    "assets/SlashScreen/testimage.png",
+                    width: 218,
+                    height: 131,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  left: 23.0,
+                  top: 10.0,
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                        height: 45,
+                        width: 45,
+                        padding: EdgeInsets.only(
+                          left: 6.0,
+                          right: 6.0,
+                          top: 3,
+                          bottom: 1.8,
+                        ),
+                        decoration:
+                        new BoxDecoration(
+                          color: Colors.white.withOpacity(0.6),
+                          borderRadius: BorderRadius.circular(13.0),
+                        ),
+                        child:Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GradientText(
+                              '10',
+                              style: const TextStyle(fontSize: 13,fontWeight: FontWeight.bold,),
+                              gradient: LinearGradient(colors: [
+                                Colors.purple,
+                                Colors.pink
+                              ]),
+                            ),
+                            GradientText(
+                              'Mai',
+                              style: const TextStyle(fontSize: 13,fontWeight: FontWeight.bold,),
+                              gradient: LinearGradient(colors: [
+                                Colors.purple,
+                                Colors.pink
+                              ]),
+                            ),
+                          ],
+                        )
+                    ),
+                    SizedBox(width: 108,),
+                    Container(
+                        height: 30,
+                        width: 30,
+                        padding: EdgeInsets.all(2),
+                        decoration:
+                        new BoxDecoration(
+                          color: Colors.white.withOpacity(0.6),
+                          borderRadius: BorderRadius.circular(7.0),
+                        ),
+                        child:Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GradientIcon(
+                              Icons.star,
+                              20.0,
+                              const LinearGradient(
+                                colors: [
+                                  Colors.purple,
+                                  Colors.pink
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.centerRight,
+                              ),
+                            ),
+                          ],
+                        )
+                    ),
+                  ],),
+              )
+            ],
+          ),
+          SizedBox(height: 14,),
+          Padding(padding: EdgeInsets.only(
+            left: 7,
+            right: 25,
+            top: 2,
+            bottom: 2,
+          ),
+            child: Text(
+              "Kanté Show Gabon ...",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          SizedBox(height: 14,),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 7,
+              right: 10,
+              top:2,
+              bottom: 2,
+            ),
+            child: Row(children: [
+              GradientIcon(
+                Icons.star,
+                15.0,
+                const LinearGradient(
+                  colors: [
+                    Colors.purple,
+                    Colors.pink
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+              GradientIcon(
+                Icons.star,
+                15.0,
+                const LinearGradient(
+                  colors: [
+                    Colors.purple,
+                    Colors.pink
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+              GradientIcon(
+                Icons.star,
+                15.0,
+                const LinearGradient(
+                  colors: [
+                    Colors.purple,
+                    Colors.pink
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+              GradientIcon(
+                Icons.star,
+                15.0,
+                const LinearGradient(
+                  colors: [
+                    Colors.purple,
+                    Colors.pink
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+              GradientIcon(
+                Icons.star,
+                15.0,
+                const LinearGradient(
+                  colors: [
+                    Colors.grey,
+                    Colors.white30,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+              SizedBox(width: 37,),
+              Text(
+                "Prix: 1000 fcfa",
+                style: TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],),
+          ),
+          SizedBox(height: 5,),
+          Padding(padding: EdgeInsets.only(
+            left: 7,
+            right: 25,
+            top: 2,
+            bottom: 2,
+          ),
+              child: Row(
+                children: [
+                  Icon(Icons.location_on_sharp,color: Colors.grey, ),
+                  Text(
+                    "Gabon",
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),),
+                ],)
+          ),
+        ],
+      )
+  );
+}
+
+class GradientText extends StatelessWidget {
+  const GradientText(
+      this.text, {
+        required this.gradient,
+        this.style,
+      });
+
+  final String text;
+  final TextStyle? style;
+  final Gradient gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (bounds) => gradient.createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      child: Text(text, style: style),
+    );
+  }
+}
+
+class GradientIcon extends StatelessWidget {
+  GradientIcon(
+      this.icon,
+      this.size,
+      this.gradient,
+      );
+
+  final IconData icon;
+  final double size;
+  final Gradient gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      child: SizedBox(
+        width: size * 1.2,
+        height: size * 1.2,
+        child: Icon(
+          icon,
+          size: size,
+          color: Colors.white,
+        ),
+      ),
+      shaderCallback: (Rect bounds) {
+        final Rect rect = Rect.fromLTRB(0, 0, size, size);
+        return gradient.createShader(rect);
+      },
     );
   }
 }
