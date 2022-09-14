f47d8f52e27ca174c538158362b46911176a956d
mohamedrayelalmi <rayenalmi64@gmail.com>
2022-09-14 12:59:27 +0100

    3 point in navbar removed

--- lib/Home/navbar.dart
+++ lib/Home/navbar.dart
@@ -206,7 +206,7 @@ class _TabIconsState extends State<TabIcons> with TickerProviderStateMixin {
   void initState() {
     widget.tabIconData?.animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 400),
+      duration: const Duration(milliseconds: 200),
     )..addStatusListener((AnimationStatus status) {
       if (status == AnimationStatus.completed) {
         if (!mounted) return;
@@ -250,69 +250,6 @@ class _TabIconsState extends State<TabIcons> with TickerProviderStateMixin {
                   child: widget.tabIconData!.isSelected
                       ? widget.tabIconData!.selectedIcon
                       : widget.tabIconData!.icon,
-                ),
-                Positioned(
-                  top: 4,
-                  left: 6,
-                  right: 0,
-                  child: ScaleTransition(
-                    alignment: Alignment.center,
-                    scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-                        CurvedAnimation(
-                            parent: widget.tabIconData!.animationController!,
-                            curve: Interval(0.2, 1.0,
-                                curve: Curves.fastOutSlowIn))),
-                    child: Container(
-                      width: 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF2633C5),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  left: 6,
-                  bottom: 8,
-                  child: ScaleTransition(
-                    alignment: Alignment.center,
-                    scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-                        CurvedAnimation(
-                            parent: widget.tabIconData!.animationController!,
-                            curve: Interval(0.5, 0.8,
-                                curve: Curves.fastOutSlowIn))),
-                    child: Container(
-                      width: 4,
-                      height: 4,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF2633C5),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 6,
-                  right: 8,
-                  bottom: 0,
-                  child: ScaleTransition(
-                    alignment: Alignment.center,
-                    scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-                        CurvedAnimation(
-                            parent: widget.tabIconData!.animationController!,
-                            curve: Interval(0.5, 0.6,
-                                curve: Curves.fastOutSlowIn))),
-                    child: Container(
-                      width: 6,
-                      height: 6,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF2633C5),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
                 ),
               ],
             ),
